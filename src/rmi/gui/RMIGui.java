1eefa79e0fa189dce27ad3f0f8f557ec62abff7e
Michael Neely <michaeljneelysd@gmail.com>
2018-02-13 15:00:31 +0000

    connect to server

--- src/rmi/gui/RMIGui.java
+++ src/rmi/gui/RMIGui.java
@@ -1,23 +1,37 @@
 package rmi.gui;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+
+import ct414.ExamServer;
 
 
 public class RMIGui {
     JFrame frame;
     
     public static void main(String[] args) {
-        JFrame frame = new LoginFrame("Student Login");
-        
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run(){
-                frame.setSize(300,150);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
-            }
-        });
+		if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+		try {
+			Registry registry = LocateRegistry.getRegistry("159.65.23.223", 1099);
+	        ExamServer e = (ExamServer) registry.lookup("ExamServer");
+	        JFrame frame = new LoginFrame("Student Login");
+	        SwingUtilities.invokeLater(new Runnable(){
+	            public void run(){
+	                frame.setSize(300,150);
+	                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	                frame.setVisible(true);
+	            }
+	        });
+		} catch (Exception e) {
+	        System.err.println("Didnt Work!");
+	        e.printStackTrace();
+	    }
         
         //Receive details about the user from this and us it to get that users assignments frame
         
