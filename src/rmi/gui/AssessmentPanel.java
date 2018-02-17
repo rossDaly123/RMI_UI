e29dbd281c4290570279bb7334fbe2053cc0f457
Michael Neely <michaeljneelysd@gmail.com>
2018-02-17 19:38:17 +0000

    correctly select option

--- src/rmi/gui/AssessmentPanel.java
+++ src/rmi/gui/AssessmentPanel.java
@@ -18,6 +18,8 @@ import javax.swing.JRadioButton;
 import javax.swing.event.EventListenerList;
 
 import ct414.Assessment;
+import ct414.InvalidOptionNumber;
+import ct414.InvalidQuestionNumber;
 import ct414.NoMatchingAssessment;
 import ct414.Question;
 import ct414.UnauthorizedAccess;
@@ -40,31 +42,44 @@ class AssessmentPanel extends JPanel {
         GridBagConstraints gc = new GridBagConstraints();
         gc.weightx = .5;
         gc.weighty = .5;
-        ArrayList<JRadioButton> groupList = new ArrayList();
         for(int i=0; i < questions.size(); i++){
             JLabel ques1 = new JLabel(questions.get(i).getQuestionDetail());
             gc.gridx = 0;
             gc.gridy = i+1;
             add(ques1, gc);
-            ButtonGroup group2 = new ButtonGroup();     //create radio group for this question
+            ButtonGroup group = new ButtonGroup();     //create radio group for this question
             gc.anchor = GridBagConstraints.LINE_START;
             gc.gridy = i+1;
             for(int j=0; j< questions.get(i).getAnswerOptions().length; j++){
                 gc.gridx = j+1;
                 JRadioButton radioBtn = new JRadioButton(questions.get(i).getAnswerOptions()[j]);
-                groupList.add(radioBtn);  
+                final int questionIndex = i;
+                final int answerIndex = j;
+                radioBtn.addActionListener(new ActionListener() {
+                		public void actionPerformed(ActionEvent e){
+                			if(radioBtn.isSelected()) {
+                				try {
+									RMIGui.workingAssessment.selectAnswer(questionIndex, answerIndex);
+								} catch (InvalidQuestionNumber e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								} catch (InvalidOptionNumber e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+                			}
+                		}
+                });
                 add(radioBtn, gc);
-                //add radio button to group
-                group2.add(radioBtn);
+                group.add(radioBtn);
             }
-            //radioBtn.isSelected();
         }
 
         JButton submitBtn = new JButton("Submit");   
         submitBtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 try {
-					RMIGui.server.submitAssessment(RMIGui.token, RMIGui.workingAssessment);
+					RMIGui.server.submitAssessment(RMIGui.token, (Assessment) RMIGui.workingAssessment);
 				} catch (RemoteException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
